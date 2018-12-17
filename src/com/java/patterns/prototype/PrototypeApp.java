0338f31a529792779d1372bc4de60157a1d2ec65
Oleksandr Kozlov <Oleksandr.Kozlov@halliburton.com>
2018-12-17 12:30:32 +0200

    prototype pattern example

---
+++ src/com/java/patterns/prototype/PrototypeApp.java
@@ -0,0 +1,111 @@
+package com.java.patterns.prototype;
+
+import java.util.Hashtable;
+
+public class PrototypeApp {
+
+	public static void main(String[] args) {
+		ShapeCache.loadCache();
+
+	      Shape clonedShape = (Shape) ShapeCache.getShape("1");
+	      System.out.println("Shape : " + clonedShape.getType());		
+
+	      Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+	      System.out.println("Shape : " + clonedShape2.getType());		
+
+	      Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+	      System.out.println("Shape : " + clonedShape3.getType());	
+	}
+}
+
+abstract class Shape implements Cloneable {
+	private String id;
+	protected String type;
+	
+	abstract void draw();
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	public Object clone() {
+		Object clone = null;
+		
+		try {
+			clone = super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return clone;
+	}
+}
+
+class Rectangle extends Shape {
+	
+	public Rectangle() {
+		type = "Rectangle";
+	}
+
+	@Override
+	void draw() {
+		System.out.println("Inside Rectangle::draw() method.");
+	}
+}
+
+class Square extends Shape {
+	
+	public Square() {
+		type = "Square";
+	}
+
+	@Override
+	void draw() {
+		System.out.println("Inside Square::draw() method.");
+	}
+}
+
+class Circle extends Shape {
+	
+	public Circle() {
+		type = "Circle";
+	}
+
+	@Override
+	void draw() {
+		System.out.println("Inside Circle::draw() method.");
+	}
+}
+
+class ShapeCache {
+	private static Hashtable<String, Shape> shapeMap = new Hashtable<String, Shape>();
+	
+	public static Shape getShape(String shapeId) {
+		Shape cachedShape = shapeMap.get(shapeId);
+		return (Shape) cachedShape.clone();
+	}
+	
+	// for each shape run database query and create shape
+	   // shapeMap.put(shapeKey, shape);
+	   // for example, we are adding three shapes
+	public static void loadCache() {
+		Circle circle = new Circle();
+		circle.setId("1");
+		shapeMap.put(circle.getId(), circle);
+		
+		Square square = new Square();
+		square.setId("2");
+		shapeMap.put(square.getId(), square);
+		
+		Rectangle rectangle = new Rectangle();
+		rectangle.setId("3");
+		shapeMap.put(rectangle.getId(), rectangle);
+	}
+}
