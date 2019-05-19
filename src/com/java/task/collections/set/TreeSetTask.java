7b0a3d1d5a77e87f3c2d662129f374b9c2aeafa4
Oleksandr Kozlov <dp070782kos@gmail.com>
2019-05-19 20:07:01 +0300

    TreeSet task

---
+++ src/com/java/task/collections/set/TreeSetTask.java
@@ -0,0 +1,25 @@
+package com.java.task.collections.set;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetTask {
+	
+	public static void main(String[] args){
+		
+		// In TreeSet uniqueness is ensured by compare, in Set by equals
+		Set<Integer> set = new TreeSet<>(new Comparator<Integer>(){
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return 0;
+			}
+		});
+		
+		set.add(1);
+		set.add(2);
+		set.add(1);	
+		
+		set.stream().forEach(c -> System.out.println(" val: " + c.intValue()));
+	}
+}
