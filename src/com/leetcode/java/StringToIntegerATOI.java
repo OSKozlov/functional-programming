11dde69ee736300c8cffb0b387e009a1373ddb7e
Oleksandr Kozlov <dp070782kos@gmail.com>
2020-01-05 14:55:29 +0200

    refactor + tests

--- src/com/leetcode/java/StringToIntegerATOI.java
+++ src/com/leetcode/java/StringToIntegerATOI.java
@@ -3,7 +3,8 @@ package com.leetcode.java;
 public class StringToIntegerATOI {
 
 	public static void main(String[] args) {
-		String str = "2147483648";//"+1";//"-91283472332";//"    +0a32";//"     +004500";//"+1";//"    +0a32";//"  -0012a42";//"3.14159";//"words and 987";//"42"; //"-91283472332";//"   -42";//"-4193 with words";
+		String str = "2147483648";//"-91283472332";//"    +0a32";//"     +004500";//"+1";//"    +0a32";//"  -0012a42";//"3.14159";//"words and 987";//"42"; //"-91283472332";//"   -42";//"-4193 with words";
+//		Integer.parseInt(str);
 		int result = myAtoi(str);
 		System.err.println("### result = " + result);
 	}
@@ -12,10 +13,9 @@ public class StringToIntegerATOI {
 		if (str == null || str.isEmpty()) return 0;
 		boolean negative = false;
 		int len = str.length();
-		int radix = 10;
 		int result = 0;
-		int limit = Integer.MAX_VALUE;
-		int multmin = limit / radix;
+		int limit = -Integer.MAX_VALUE;
+		int radix = 10;
 		int i = 0;
 		if (len > 0) {
 			char firstChar = str.charAt(0);
@@ -23,11 +23,13 @@ public class StringToIntegerATOI {
 				if (i == len) return 0;
 				firstChar = str.charAt(i++);
 			}
-			if (str.charAt(i) == '+' && len==1) return 0;
-			if (str.charAt(i) == '-' && len==1) return 0;
-			if (firstChar == '+' && str.charAt(i+1) == '0' && len == 2) return 0;
-			if (str.charAt(i) == '-' && str.charAt(i+1) == '+') return 0;
-			if (str.charAt(i) == '+' && str.charAt(i+1) == '-') return 0;
+			if ((str.charAt(i) == '+' && len == 1) || (str.charAt(i) == '-' && len == 1))
+				return 0;
+			if (firstChar == '+' && str.charAt(i + 1) == '0' && len == 2)
+				return 0;
+			if ((str.charAt(i) == '-' && str.charAt(i + 1) == '+')
+					|| ((str.charAt(i) == '+' && str.charAt(i + 1) == '-')))
+				return 0;
 			if (firstChar != '-' && Character.isAlphabetic(firstChar))
 				return 0;
 			if (firstChar < '0') {
@@ -36,25 +38,30 @@ public class StringToIntegerATOI {
 					limit = Integer.MIN_VALUE;
 				}
 			}
+			int multmin = limit / radix;
 			while(i < len) {
 				char ch = str.charAt(i++);
-				/* if (ch == '+') return 0; */
 				if (ch == '.') return negative ? result : -result;
 				if (!Character.isDigit(ch) && ch!='-' && ch!='+') {
 					return negative ? result : -result;
 				}
 				if (result == 0 && !Character.isDigit(ch)) continue;
 				int digit = Character.digit(ch, radix);
-				result *= radix;
-				if (negative && result < multmin) {
-					result = Integer.MIN_VALUE;
+				if (result < multmin) {
+					System.err.println("### test ###");
+					if (result<=Integer.MIN_VALUE) {
+						result = result - digit;
+					} else {
+						result = Integer.MIN_VALUE;
+					}
 					break;
 				}
+				result *= radix;
 				result -= digit;
 			}
-			if (result < Integer.MIN_VALUE) {
-				result = Integer.MIN_VALUE;
-			}
+			/*
+			 * if (result<=Integer.MAX_VALUE) { result = Integer.MAX_VALUE; return result; }
+			 */
 		}
 		return negative ? result : -result;
     }
---
+++ src/com/leetcode/test/StringToIntegerATOITest.java
@@ -0,0 +1,42 @@
+package com.leetcode.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.leetcode.java.StringToIntegerATOI;
+
+class StringToIntegerATOITest {
+	
+	@Test
+	void testSign() {
+		StringToIntegerATOI atoi = new StringToIntegerATOI();
+		assertEquals(1, atoi.myAtoi("+1"));
+		assertEquals(0, atoi.myAtoi("    +0a32"));
+		assertEquals(4500, atoi.myAtoi("     +004500"));
+		//assertEquals(, atoi.myAtoi("  -0012a42"));
+		assertEquals(-42, atoi.myAtoi("   -42"));
+		assertEquals(-4193, atoi.myAtoi("-4193 with words"));
+		assertEquals(0, atoi.myAtoi("-"));
+		assertEquals(0, atoi.myAtoi("+"));
+		assertEquals(0, atoi.myAtoi("+-"));
+		assertEquals(0, atoi.myAtoi("-+"));
+	}
+	
+	@Test
+	void testSignless() {
+		StringToIntegerATOI atoi = new StringToIntegerATOI();
+		assertEquals(3, atoi.myAtoi("3.14159"));
+		assertEquals(0, atoi.myAtoi("words and 987"));
+		assertEquals(42, atoi.myAtoi("42"));
+	}
+
+	@Test
+	void testMinMax() {
+		StringToIntegerATOI atoi = new StringToIntegerATOI();
+		assertEquals(-2147483648, atoi.myAtoi("-91283472332"));
+		assertEquals(2147483646, atoi.myAtoi("2147483646"));
+		assertEquals(-2147483647, atoi.myAtoi("-2147483647"));
+//		assertEquals(2147483647, atoi.myAtoi("2147483648"));	
+	}
+}
