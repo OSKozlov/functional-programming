3f380369da1624d755e43ad6899a11bf0a19e61d
dp070782kos <dp070782kos@gmail.com>
2020-06-30 23:25:39 +0300

    refactor

--- src/com/leetcode/java/MergeSortedArray.java
+++ src/com/leetcode/java/MergeSortedArray.java
@@ -3,9 +3,6 @@ package com.leetcode.java;
 public class MergeSortedArray {
 	
 	public static void main(String[] args) {
-		/*
-		 * int[] nums1 = {1,2,3,0,0,0}; int m = 3; int[] nums2 = {2,5,6}; int n = 3;
-		 */
 		
 		int[] nums1 = {0}; 
 	    int m = 0;
@@ -21,10 +18,6 @@ public class MergeSortedArray {
 
 	private static void merge(int[] nums1, int m, int[] nums2, int n) {
 		
-		if (nums1.length == 1 && nums1[0] == 0 && nums2.length == 1 && nums2[0] == 1) {
-			nums1[0] = 1;
-		};
-		
 		int last = m + n - 1;
 		int i = m - 1;
 		int j = n -1;
@@ -34,7 +27,7 @@ public class MergeSortedArray {
 		while(i>=0)
 			nums1[last--] = nums1[i--];
 		while(j>=0)
-			nums2[last--] = nums2[j--];
+			nums1[last--] = nums2[j--];
         
     }
 }
